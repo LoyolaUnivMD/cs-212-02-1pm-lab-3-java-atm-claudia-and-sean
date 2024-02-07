52c25b977678b3e15807a59a168cdccadf19a3b9
seantriplett <srtriplett@loyola.edu>
2024-02-07 13:49:46 -0500

    withdraw and deposit added

--- Lab3.java
+++ Lab3.java
@@ -11,8 +11,40 @@
 import java.util.Scanner;
 
 class Lab3 {
+    //Initialize balance
+    private double balance;
+
+    public Lab3() {
+        //Initialize balance as 212.9
+        balance = 212.9;
+    }
+    public void deposit(double amount) {
+        //Add user generated number to balance
+        balance+=amount;
+    }
+    public void withdraw(double amount) {
+        //If user generated number is less than or equal to balance
+        if (amount <= balance) {
+            //Subtract user generated number from balance
+            balance-=amount;
+        }
+        else {
+            //Output error message
+            System.out.println("Sorry, you have insufficient funds for this withdraw.");
+        }
+    }
+    public String menuOption() {
+        System.out.println();
+    }
+    //1. Loop until user generated string is a valid option of four options (deposit, check balance, withdraw, or leave)
+    //
+    //Check Number:
+    //1. Loop until user generated number is a valid and positive number
+    //
+    //Receipt option:
+    //1. Loop until user generated string is either printed or emailed
     public static void main(String[] args) {
-       System.out.println("Welcome to Lab 3 test");
+
 
 
     }
